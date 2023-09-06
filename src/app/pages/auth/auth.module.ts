62446b8d71376640a5e5c8252e191e34ae8972a2
Leonardo Natal <leonardo.natal@dev.pro>
2023-09-06 18:11:03 -0300

    env config

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from '@app/pages/auth/login/login.component';
+import { SignupComponent } from '@app/pages/auth/signup/signup.component';
+import { AuthRoutingModule } from '@app/pages/auth/auth-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [SignupComponent, LoginComponent],
   imports: [CommonModule, AuthRoutingModule],
 })
 export class AuthModule {}
