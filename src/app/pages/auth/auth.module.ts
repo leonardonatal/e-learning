7eec23021cd1c837d1a3b2abca842f4509a2ebab
Leonardo Natal <leonardo.natal@dev.pro>
2023-09-09 09:28:36 -0300

    angular material  main config

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from '@app/pages/auth/login/login.component';
 import { SignupComponent } from '@app/pages/auth/signup/signup.component';
-import { AuthRoutingModule } from '@app/pages/auth/auth-routing.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MatToolbarModule],
 })
 export class AuthModule {}
